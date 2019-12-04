7c5b411fec7127d0d4edf05b1c5493703096d9e8
Anthony Obando <obandoanthony.8@gmail.com>
2019-12-04 00:24:02 +0000

    Finalizing set up

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var fetch = require('node-fetch');
+var app = express();
+var port = process.env.PORT|| 3000;
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function(){
+});
